cb46d1d56a2c42a562fc590ad2422478e6d016d5
David L. <dlouzao.code@gmail.com>
2024-12-13 15:33:14 +0000

    cambio 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write('hola')
+
+x = 10
+
+st.write(f"Mi numero es {x}")
